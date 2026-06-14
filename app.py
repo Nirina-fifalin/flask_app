57c4eb7631da805d9c3fb70865bc8063f9b3a334
Nirina-fifalin <nirin.fifalin@gmail.com>
2026-06-14 15:05:08 +0300

    Ajout route /bonjour

--- app.py
+++ app.py
@@ -5,11 +5,15 @@ app = Flask(__name__)
 @app.route('/')
 @app.route('/hello/')
 def hello_world():
-    return 'Hello World!!\n'
+    return 'Hello World!\n'
 
 @app.route('/hello/<username>')
 def hello_user(username):
     return 'Hello %s!\n' % username
+
+@app.route('/bonjour/<username>')
+def bonjour_user(username):
+    return 'Bonjour %s!\n' % username
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
