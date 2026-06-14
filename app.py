5226d200ec55bfe4cd55c68faec1b6efa163fb99
Nirina-fifalin <nirin.fifalin@gmail.com>
2026-06-14 15:55:09 +0300

    Suppression route /bonjour

--- app.py
+++ app.py
@@ -11,10 +11,6 @@ def hello_world():
 def hello_user(username):
     return 'Hello %s!\n' % username
 
-@app.route('/bonjour/<username>')
-def bonjour_user(username):
-    return 'Bonjour %s!\n' % username
-
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
 
